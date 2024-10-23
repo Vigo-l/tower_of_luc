4a983fb5fdb15c1ab270f8a74d91fed7145bdc4b
spongmuizs <128585576+spongmuizs@users.noreply.github.com>
2024-10-23 12:10:18 +0200

    begin tutorial and new floor button animationthing

--- Assets/Scripts/BoxCheckerScript.cs
+++ Assets/Scripts/BoxCheckerScript.cs
@@ -1,8 +1,18 @@
+using JetBrains.Annotations;
+using System.Collections;
 using UnityEngine;
 
 public class BoxCheckerScript : MonoBehaviour
 {
     public GameObject doorDoor;
+    public GameObject lockedDoor;
+    public GameObject doorcamera;
+    public GameObject playercamera;
+    public Animation dooranimate;
+    public AnimationClip dooranimation;
+
+    public bool playAnimation;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +27,26 @@ public class BoxCheckerScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("HelpCube"))
+        if (playAnimation)
         {
-            doorDoor.SetActive(false);
+            doorcamera.SetActive(true);
+
+
+            dooranimate.Play();
+            StartCoroutine(DoorANIMATION());
         }
+        else
+        {
+            if (other.gameObject.CompareTag("HelpCube"))
+            {
+                doorDoor.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator DoorANIMATION()
+    {
+        yield return new WaitForSeconds(dooranimation.length);
+        doorcamera.SetActive(false);
     }
 }
--- Assets/Scripts/Grabbable.cs
+++ Assets/Scripts/Grabbable.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Grabbable : MonoBehaviour
 {
     private Rigidbody objectRB;
     private Transform grabPointTransform;
+
+    [SerializeField] private bool destroyGateOnPickup;
+    [SerializeField] private GameObject gateObject;
+    [SerializeField] private AudioSource gateSound;
+
+    [SerializeField] private bool changeCubeText;
+    [SerializeField] private TextMeshProUGUI cubeText;
+    [SerializeField] private string dropText;
+    [SerializeField] private string pickText;
+
+    private bool destroyed;
 
     private void Awake()
     {
@@ -16,12 +28,29 @@ public class Grabbable : MonoBehaviour
     {
         this.grabPointTransform = grabPointTransform;
         objectRB.useGravity = false;
+
+        if (destroyGateOnPickup && !destroyed)
+        {
+            Destroy(gateObject);
+            gateSound.Play();
+            destroyed = true;
+        }
+
+        if (changeCubeText)
+        {
+            cubeText.text = pickText;
+        }
     }
 
     public void Drop()
     {
         this.grabPointTransform = null;
         objectRB.useGravity = true;
+
+        if (changeCubeText)
+        {
+            cubeText.text = dropText;
+        }
     }
 
     private void FixedUpdate()
@@ -32,5 +61,11 @@ public class Grabbable : MonoBehaviour
             Vector3 newPosition = Vector3.Lerp(transform.position, grabPointTransform.position, Time.deltaTime * lerpSpeed);
             objectRB.MovePosition(grabPointTransform.position);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!collision.gameObject.CompareTag("Floor"))
+            Drop();
     }
 }
