b14ed45ac0faee6e1df191ee334d0050f5a921a9
spongmuizs <128585576+spongmuizs@users.noreply.github.com>
2024-10-24 01:24:02 +0200

    you can now win the gane

--- Assets/Scripts/IntegralePickup.cs
+++ Assets/Scripts/IntegralePickup.cs
@@ -11,22 +11,25 @@ public class IntegralePickup : MonoBehaviour
     [SerializeField] GameObject normalMusic;
     [SerializeField] GameObject glitchMusic;
 
+    [SerializeField] GameObject hiddenDoor;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            GameObject[] lights = GameObject.FindGameObjectsWithTag("Light");
+            GameObject[] lights = GameObject.FindGameObjectsWithTag("Light"); //turn all lights red
             foreach (GameObject l in lights)
                 l.GetComponent<Light>().color = Color.red;
 
-            integraler.SetActive(true);
+            integraler.SetActive(true); // spawn integraler
 
-            DataManager.escapeSequence = true;
+            DataManager.escapeSequence = true; //turn on global escapesequence value which handles a bunch of shit
 
-            normalMusic.SetActive(false);
+            normalMusic.SetActive(false); //switch music
             glitchMusic.SetActive(true);
 
-            Destroy(integraleLight);
+            Destroy(integraleLight); // destroy the light emmiting from the integrale, the integrale and open the door to the ending
+            Destroy(hiddenDoor);
             Destroy(gameObject);
         }
     }
